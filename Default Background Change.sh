6004ab4b816b4c8920d4d9a32a065c549f236e15
rthaley <39775381+rthaley@users.noreply.github.com>
2021-05-06 17:45:15 -0400

    Delete Default Background Change.sh

--- Default Background Change.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-# Renames /Library/Desktop Pictures/High Sierra.jpg to oldHigh Sierra.jpg
-# Renames /Library/Desktop Pictures/Sierra 2.jpg to High Sierra.jpg
-# This should, in theory, upon reboot, make Sierra 2 the default desktop background.
-# This will probably break in the next Major (10.14 etc) release of the OS. YMMV
-# SCOUT IT ryne@wearescout.com 
